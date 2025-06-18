9fb69eca5c70195d27f3987292b8466cefcedad3
Cristhian Rodríguez <crisrodam1996@gmail.com>
2025-06-18 17:39:00 -0500

    Database configuration

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,10 @@
-import 'dotenv/config'
-import { bcryptUtil } from '../utils/index.utils.js'
+import "dotenv/config";
+import { bcryptUtil } from "../utils/index.utils.js";
 
 const {
   PORT = 3000,
   LOCAL_DATABASE_URI,
+  DEV_DATABASE_URI,
   SECRET_WORD,
   CLOUDINARY_KEY,
   CLOUDINARY_NAME,
@@ -13,20 +14,43 @@ const {
   ADMIN_PASSWORD,
   OPENAI_API_KEY,
   HF_API_TOKEN,
-} = process.env
+  NODE_ENV,
+} = process.env;
 
 const NODEMAILER_CONFIG = {
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   port: 465,
   secure: true, // Use `true` for port 465, `false` for all other ports
   auth: {
     user: GMAIL_USER,
     pass: GMAIL_PASSWORD,
   },
-}
+};
+
+const CONNECTION = {
+  URI: NODE_ENV === "development" ? DEV_DATABASE_URI : LOCAL_DATABASE_URI,
+  CONFIG:
+    NODE_ENV !== "development"
+      ? {
+          logging: false,
+          native: false,
+          dialect: "postgres",
+          dialectOptions: {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false,
+            },
+          },
+        }
+      : {
+          logging: false,
+          native: false,
+          dialect: "postgres",
+        },
+};
 
 export {
-  LOCAL_DATABASE_URI,
+  CONNECTION,
   PORT,
   SECRET_WORD,
   CLOUDINARY_KEY,
@@ -36,4 +60,4 @@ export {
   ADMIN_PASSWORD,
   OPENAI_API_KEY,
   HF_API_TOKEN,
-}
+};
--- src/database/database.js
+++ src/database/database.js
@@ -1,13 +1,10 @@
-import { Sequelize } from 'sequelize'
-import { LOCAL_DATABASE_URI } from '../config/config.js'
-import { models } from '../models/index.model.js'
+import { Sequelize } from "sequelize";
+import { CONNECTION } from "../config/config.js";
+import { models } from "../models/index.model.js";
 
-const sequelize = new Sequelize(LOCAL_DATABASE_URI, {
-  native: false,
-  logging: false,
-})
+const sequelize = new Sequelize(CONNECTION.URI, CONNECTION.CONFIG);
 
-models.forEach((model) => model(sequelize))
+models.forEach((model) => model(sequelize));
 
 const {
   About,
@@ -28,68 +25,68 @@ const {
   UserLanguage,
   UserReference,
   UserSkill,
-} = sequelize.models
+} = sequelize.models;
 
-User.hasOne(About, { foreignKey: 'UserId' })
-About.belongsTo(User, { foreignKey: 'UserId' })
+User.hasOne(About, { foreignKey: "UserId" });
+About.belongsTo(User, { foreignKey: "UserId" });
 
 // Relaciones
-User.hasMany(Code, { foreignKey: 'UserId' })
-Code.belongsTo(User, { foreignKey: 'UserId' })
+User.hasMany(Code, { foreignKey: "UserId" });
+Code.belongsTo(User, { foreignKey: "UserId" });
 
-User.hasMany(Employee, { foreignKey: 'UserId' })
-Employee.belongsTo(User, { foreignKey: 'UserId' })
+User.hasMany(Employee, { foreignKey: "UserId" });
+Employee.belongsTo(User, { foreignKey: "UserId" });
 
-User.hasMany(Interest, { foreignKey: 'UserId' })
-Interest.belongsTo(User, { foreignKey: 'UserId' })
+User.hasMany(Interest, { foreignKey: "UserId" });
+Interest.belongsTo(User, { foreignKey: "UserId" });
 
-User.hasMany(JobApplication, { foreignKey: 'UserId' })
-JobApplication.belongsTo(User, { foreignKey: 'UserId' })
+User.hasMany(JobApplication, { foreignKey: "UserId" });
+JobApplication.belongsTo(User, { foreignKey: "UserId" });
 
-Message.belongsTo(User, { as: 'Sender', foreignKey: 'SenderId' })
-Message.belongsTo(User, { as: 'Receiver', foreignKey: 'ReceiverId' })
+Message.belongsTo(User, { as: "Sender", foreignKey: "SenderId" });
+Message.belongsTo(User, { as: "Receiver", foreignKey: "ReceiverId" });
 
-User.hasMany(Message, { as: 'SentMessages', foreignKey: 'SenderId' })
-User.hasMany(Message, { as: 'ReceivedMessages', foreignKey: 'ReceiverId' })
+User.hasMany(Message, { as: "SentMessages", foreignKey: "SenderId" });
+User.hasMany(Message, { as: "ReceivedMessages", foreignKey: "ReceiverId" });
 
-User.hasMany(Notification, { foreignKey: 'UserId' })
-Notification.belongsTo(User, { foreignKey: 'UserId' })
+User.hasMany(Notification, { foreignKey: "UserId" });
+Notification.belongsTo(User, { foreignKey: "UserId" });
 
-User.hasMany(Resume, { foreignKey: 'UserId' })
-Resume.belongsTo(User, { foreignKey: 'UserId' })
+User.hasMany(Resume, { foreignKey: "UserId" });
+Resume.belongsTo(User, { foreignKey: "UserId" });
 
-User.hasMany(UserCertification, { foreignKey: 'UserId' })
-UserCertification.belongsTo(User, { foreignKey: 'UserId' })
+User.hasMany(UserCertification, { foreignKey: "UserId" });
+UserCertification.belongsTo(User, { foreignKey: "UserId" });
 
-User.hasMany(UserEducation, { foreignKey: 'UserId' })
-UserEducation.belongsTo(User, { foreignKey: 'UserId' })
+User.hasMany(UserEducation, { foreignKey: "UserId" });
+UserEducation.belongsTo(User, { foreignKey: "UserId" });
 
-User.hasMany(UserExperience, { foreignKey: 'UserId' })
-UserExperience.belongsTo(User, { foreignKey: 'UserId' })
+User.hasMany(UserExperience, { foreignKey: "UserId" });
+UserExperience.belongsTo(User, { foreignKey: "UserId" });
 
-User.hasMany(UserLanguage, { foreignKey: 'UserId' })
-UserLanguage.belongsTo(User, { foreignKey: 'UserId' })
+User.hasMany(UserLanguage, { foreignKey: "UserId" });
+UserLanguage.belongsTo(User, { foreignKey: "UserId" });
 
-User.hasMany(UserReference, { foreignKey: 'UserId' })
-UserReference.belongsTo(User, { foreignKey: 'UserId' })
+User.hasMany(UserReference, { foreignKey: "UserId" });
+UserReference.belongsTo(User, { foreignKey: "UserId" });
 
-User.hasMany(UserSkill, { foreignKey: 'UserId' })
-UserSkill.belongsTo(User, { foreignKey: 'UserId' })
+User.hasMany(UserSkill, { foreignKey: "UserId" });
+UserSkill.belongsTo(User, { foreignKey: "UserId" });
 
-Branch.hasMany(Interest, { foreignKey: 'BranchId' })
-Interest.belongsTo(Branch, { foreignKey: 'BranchId' })
+Branch.hasMany(Interest, { foreignKey: "BranchId" });
+Interest.belongsTo(Branch, { foreignKey: "BranchId" });
 
-Branch.hasMany(JobOffer, { foreignKey: 'BranchId' })
-JobOffer.belongsTo(Branch, { foreignKey: 'BranchId' })
+Branch.hasMany(JobOffer, { foreignKey: "BranchId" });
+JobOffer.belongsTo(Branch, { foreignKey: "BranchId" });
 
-Branch.hasMany(Notification, { foreignKey: 'BranchId' })
-Notification.belongsTo(Branch, { foreignKey: 'BranchId' })
+Branch.hasMany(Notification, { foreignKey: "BranchId" });
+Notification.belongsTo(Branch, { foreignKey: "BranchId" });
 
-Branch.hasMany(Employee, { foreignKey: 'BranchId' })
-Employee.belongsTo(Branch, { foreignKey: 'BranchId' })
+Branch.hasMany(Employee, { foreignKey: "BranchId" });
+Employee.belongsTo(Branch, { foreignKey: "BranchId" });
 
-JobOffer.hasMany(JobApplication, { foreignKey: 'JobOfferId' })
-JobApplication.belongsTo(JobOffer, { foreignKey: 'JobOfferId' })
+JobOffer.hasMany(JobApplication, { foreignKey: "JobOfferId" });
+JobApplication.belongsTo(JobOffer, { foreignKey: "JobOfferId" });
 
 export {
   sequelize,
@@ -111,4 +108,4 @@ export {
   UserLanguage,
   UserReference,
   UserSkill,
-}
+};
